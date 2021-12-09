5925c18dd4f1620e500d0885fe53798f4de7302f
Vovan83 <64544816+Vovan83@users.noreply.github.com>
2021-12-09 19:06:07 +0500

    added a basic controller for admin and api projects

---
+++ Api/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Admin.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public string GetUserId()
+        {
+            if (User == null || !User.HasClaim(x => x.Type == ClaimTypes.NameIdentifier))
+                return string.Empty;
+            return User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
