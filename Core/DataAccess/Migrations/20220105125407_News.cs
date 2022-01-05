3fa6cdc1b5b49690211f3e9f1398a1e97f5bbb3e
Azizjan <azizjan.ayupov@gmail.com>
2022-01-05 18:55:06 +0600

    news migration

---
+++ Core/DataAccess/Migrations/20220105125407_News.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Core.DataAccess.Migrations
+{
+    public partial class News : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NewsTranslationId",
+                table: "Medias",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AuthorId = table.Column<string>(type: "text", nullable: true),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    EditorId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_News_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_News_AspNetUsers_EditorId",
+                        column: x => x.EditorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsTranslations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Language = table.Column<short>(type: "smallint", nullable: false),
+                    NewsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsTranslations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NewsTranslations_News_NewsId",
+                        column: x => x.NewsId,
+                        principalTable: "News",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medias_NewsTranslationId",
+                table: "Medias",
+                column: "NewsTranslationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_AuthorId",
+                table: "News",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_EditorId",
+                table: "News",
+                column: "EditorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsTranslations_NewsId",
+                table: "NewsTranslations",
+                column: "NewsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Medias_NewsTranslations_NewsTranslationId",
+                table: "Medias",
+                column: "NewsTranslationId",
+                principalTable: "NewsTranslations",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Medias_NewsTranslations_NewsTranslationId",
+                table: "Medias");
+
+            migrationBuilder.DropTable(
+                name: "NewsTranslations");
+
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Medias_NewsTranslationId",
+                table: "Medias");
+
+            migrationBuilder.DropColumn(
+                name: "NewsTranslationId",
+                table: "Medias");
+        }
+    }
+}
