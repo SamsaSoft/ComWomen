d1c499485f3312ce7c78a2ab30bc772050f22256
Azizjan <azizjan.ayupov@gmail.com>
2022-01-05 17:25:43 +0600

    temp

---
+++ Core/DataAccess/Entities/NewsTranslation.cs
@@ -0,0 +1,19 @@
+﻿using Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.DataAccess.Entities
+{
+    public class NewsTranslation
+    {
+        public int Id { get; set; }
+        [MaxLength(128), Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public List<Media> Media { get; set; }
+        public Language Language { get; set; }
+
+        public int NewsId { get; set; }
+        public News News { get; set; }
+    }
+}
